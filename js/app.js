67cb222074123e99588a7f21c4e51c2f79b77f6e
sahilkhurana02 <sahil_khurana@outlook.com>
2017-03-22 21:26:01 +0530

    Applied Boundries

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function(x,y) {
+var Enemy = function(x,y, step) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
+    this.step = step;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -16,7 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += 20*dt;
+    this.x += this.step*dt;
+    if(this.x > 500) {
+        this.x = -100;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -34,7 +38,10 @@ var Player = function(x, y) {
     this.y = y;
 };
 Player.prototype.update = function(x, y, dt) {
-    
+    if(this.y < 0) {
+        this.y = 300;
+    }
+
 
     
 };
@@ -45,16 +52,19 @@ Player.prototype.handleInput = function(e) {
 
     switch (e) {
         case "left":
-            this.x -= 100;
+            if(this.x > 0)
+                this.x -= 100;
             break;
         case "up":
             this.y -= 85;
             break;
         case "right":
-            this.x += 100; 
+            if(this.x < 400)
+                this.x += 100; 
             break;
         case "down":
-            this.y += 85;
+            if(this.y < 380)
+                this.y += 85;
             break;
     }        
  
@@ -63,7 +73,7 @@ Player.prototype.handleInput = function(e) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-allEnemies = [/*new Enemy(200, 200)*/];
+allEnemies = [new Enemy(-100, 215, 15), new Enemy(-100, 130, 25), new Enemy(-100, 50, 40)];
 player = new Player(200, 300);
 
 // This listens for key presses and sends the keys to your
